916b59ca2e0785be85528aec351a51bf83e240e7
sns-jia <jia@summernightstudios.com>
2024-02-12 22:41:07 -0500

    Loading DAT files
    Loading DAT files in TileWorld, uploading txt and png files.

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -2,7 +2,9 @@ import cc_dat_utils
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
+level = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
 
+print(level)
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
