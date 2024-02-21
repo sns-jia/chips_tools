fe9b0385c87fc90d605c9d602bcbe5422baeb555
sns-jia <jia@summernightstudios.com>
2024-02-21 11:35:04 -0500

    Progress on assignment "Chip's Level JSON"
    The initial progress to write out the structures

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,6 +1,20 @@
 import cc_dat_utils
+import cc_classes
+import json
 
 #Part 3
+
 #Load your custom JSON file
+json_data = json.load(open('data/cc_level_pack.json'))
+print(json_data)
+
 #Convert JSON data to CCLevelPack
+level_pack = cc_classes.CCLevelPack()
+level1 = cc_classes.CCLevel()
+level1.num_chips = 1
+level1.level_number = 1
+level1.time = 100
+level1.title = "test level"
+level_pack.add_level(level1)
 #Save converted data to DAT file
+cc_dat_utils.write_cc_level_pack_to_dat(level_pack,'data/jia_level_pack.dat')
